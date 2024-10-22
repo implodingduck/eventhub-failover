ded73e63d3ac1164d21455a4fa60a505ae69bdeb
implodingduck <zsek320@gmail.com>
2024-10-22 12:02:25 -0500

    working

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -2,22 +2,22 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/azure/azapi" {
-  version     = "1.13.1"
-  constraints = "1.13.1"
+  version     = "1.3.0"
+  constraints = "1.3.0"
   hashes = [
-    "h1:xDZG4lbtQJeyJa3Gzo8qecYxyw+AIXYcdDRlkaSLNz8=",
-    "zh:1f2aceddd67ceeb82a75c2f15dc01e54781e9aed5968507dbc29590c165b2e2b",
-    "zh:397f0bfbac899d48e23cecf38d362c27562150aa20b19157b5bd370b8e6801ee",
-    "zh:652263b7d00623684e29ef7b8ff285a17c5bd7cc8ba7d22967c66d0b3a3c568a",
-    "zh:652c53320a41434942877515780296a1509be03f32d54e60178f39200f960a67",
-    "zh:666426faf686401e54ec09fe06e9d7c06a6455ec398764f70558440c73aeb7f9",
-    "zh:6aa91ae8ba78f2494f99b4c99e66d15ed0b14d735cd1f77adc12ff9dfa075807",
-    "zh:a529e5a13c37d1805c469227f08cdbe7527d04dd64d18709d26627c6a0b588b1",
-    "zh:a589c049205e8e5bf94a13d56b28f400d908ad27e13e16df64408ee82eb8a0ff",
-    "zh:a9a50defdee230f315f74be6c77ff104fe2610a1b3ad6b87326f555e80d13b18",
-    "zh:ba49ef70d96e13795e2dbffd6cb2ff976dfe84e0373a5971ebe3b4c9c9b7af60",
-    "zh:d3ed50efe5f8c80d3d7d464ab9a13ccf82440d871c9ce3032ce476845364c6b9",
-    "zh:e3eb48ee8c36ee4f81850d8a21fc59b81886c729d7c3b7adece4a25f355bed2f",
+    "h1:b4PzksrgRiHgOTVXIMTODOAlsvdj3uWSdCvA7lw+9ik=",
+    "zh:0923b297c5b71ed584e5f3a0b2393e80244076e85102a90438159833353274b0",
+    "zh:11fa2922aa98ca55beaf7cc33c7edbde81bbd405fdfea2955276c7f5a8537240",
+    "zh:14af830fb6091d084bfc2711c8e9c7bf05aa3c56fe8fd8e2fb4eddeb345be88d",
+    "zh:25258425ecbffbdf09b0c8131d2c680cddd19b504e0036ee5f83972dcae7df0a",
+    "zh:2922b535fe4d4f0963189548f2f8360a0aaf951fd411354f2269a111d8a0c1ad",
+    "zh:32c9360305e00c25d0f9d0a84dfbdbad8da2465be769a9c1f11f132c0225358e",
+    "zh:4ddd3ee23c340d5000839d8d30ba7f94e695476d63075f95cfb041e67d8f6ef6",
+    "zh:5c1514392a5c3dd51084aa70cb6c4dcc8b027c4508b5e4eb9f8c3990fd403213",
+    "zh:6b3ecac7099ab86c007b5ad636bd029f5e5f3e9bd06b0f74c82f0451a7995ecc",
+    "zh:6cb7081745b378e910e0cf09fb5717a2ad35e629ce3e07415d6682c1c1407872",
+    "zh:7107eda5125c1b983380f1f6418c592fb7fb2eb5b589ad0e08f6c47341f36318",
+    "zh:c6fa7af32a7a47d23a85e0eea4d4cbb065378ae75aed8c9c628fb625b04bc619",
   ]
 }
 
@@ -42,22 +42,22 @@ provider "registry.terraform.io/hashicorp/azuread" {
 }
 
 provider "registry.terraform.io/hashicorp/azurerm" {
-  version     = "3.109.0"
-  constraints = "3.109.0"
+  version     = "4.6.0"
+  constraints = "4.6.0"
   hashes = [
-    "h1:rrySPpbxkcuBtlWs0cIb+vxfDKZd0XMQB7dYhfxuw8U=",
-    "zh:4324c3df26709c7e669b751259cc5e62c4694ab44370dfcdfe197dcd9261c365",
-    "zh:4e3e83649240cea7105cd2802d0ae64b143fb543c2f559173feae5a108bc4287",
-    "zh:74ebf6be1277e9bd357b011026b80fc5ec1c26b70ec7ddd5fcae5e977f9a66ef",
-    "zh:82cfd3c92035f834a05f4b91d813a059a29ff4157792e36a0b3a224cba8737ae",
-    "zh:93f05c8ae3555c885c84b82781b2e90774671c321138b7f3c38ecd498009e1d8",
-    "zh:9b445a9a1544b4b38db10fadbd9ffd5efdded0def54feb9ca593e1bec6fbec5f",
-    "zh:b21ccd2c1bc691cf2f9876482b6e226d8a37a48de951b168a10f96ba929ebefd",
-    "zh:b7b7e458eb3c22669e1d36e9ef1886272c10f310501001abce8ae76383014fa5",
-    "zh:bd3c0cf7caab0a989227934bc60a8ac27131efcf84dd77cb6e32e68374170aee",
-    "zh:f4b9ccbb28eadf3825f6d7d38a3519379de222f136235a2f21a96c0221d65fb8",
+    "h1:AYs06d5QVb992u6JKFltLSvWB5jLtIY/f6otc8Y2P6s=",
+    "zh:13107f35a7060efa62570e3482285003092ead0afa0fa9fa1b0dda4f70080cb9",
+    "zh:22703b9f318adbda7e75d50e76b345651cbf371bce227566a8b8532c9c4f0880",
+    "zh:601d1eb1c056e4de649561764056e3dc21b837d104d15fee31f9aaaacb292046",
+    "zh:6572232f9b0d20b149e8b5cf161fada6d8122ee1fea732c2f1e9402fe4d3375f",
+    "zh:6c19ab78c6cf7eb04b8db978daa611db04598fd490c90b85889d08726da1d095",
+    "zh:7f6eae5fa14def221422fdd34ea122ea0263d0c3c67eb0fd7870d664d7d7a360",
+    "zh:8530912ffcc8a92dd5c186ecd08a9c2f1282838a3ad5a71786c2866402c71da7",
+    "zh:a6b110c6cff0ed8c1a8969bd86583db1d5b4f3100f89ce0a0e0350b1ad8cd47b",
+    "zh:bd67b5d92e06bc44b166f4cabab189de5cc06506333283ac88b4ce21707b8c6b",
+    "zh:c472fdcafbca86a0a5b515378409d0bbddff6a6abb0bd0ecf438c610944b1e98",
+    "zh:ca4bb3797ddf20062995134211b50e313242c8a81aa5da63cc51a47e42f494c4",
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
-    "zh:f8ef0b4a970ff5edeadfdeed77f9d0682befdca5df4e9b6d9dcfdf9903305b26",
   ]
 }
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.109.0"
+      version = "=4.6.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -10,7 +10,7 @@ terraform {
     }
     azapi = {
       source = "azure/azapi"
-      version = "=1.13.1"
+      version = "=1.3.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -76,10 +76,12 @@ resource "azurerm_eventhub_namespace" "secondary" {
 }
 
 resource "azapi_resource" "eventhub-dr-config" {
-  type = "Microsoft.EventHub/namespaces/disasterRecoveryConfigs@2024-01-01"
+  type = "Microsoft.EventHub/namespaces/disasterRecoveryConfigs@2021-11-01"
   name = "ehnfailovertest${random_string.unique.result}"
   parent_id = azurerm_eventhub_namespace.primary.id
-  body = jsonencode({
+  #location = azurerm_resource_group.rg.location
+
+  body = jsonencode({  
     properties = {
       partnerNamespace = azurerm_eventhub_namespace.secondary.id
     }
@@ -128,12 +130,12 @@ resource "azurerm_eventhub_consumer_group" "replicator" {
 }
 
 resource "azurerm_storage_account" "primary" {
-  name                     = "saehfo${random_string.unique.result}${local.loc_for_naming}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
+  name                            = "saehfo${random_string.unique.result}${local.loc_for_naming}"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  
   tags = local.tags
 }
 
@@ -161,21 +163,7 @@ resource "azurerm_storage_container" "primary-replicator" {
   container_access_type = "private"
 }
 
-resource "azurerm_storage_account" "secondary" {
-  name                     = "saehfo${random_string.unique.result}${local.loc_for_naming2}"
-  resource_group_name      = azurerm_resource_group.rg2.name
-  location                 = azurerm_resource_group.rg2.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 
-  tags = local.tags
-}
-
-resource "azurerm_storage_container" "secondary" {
-  name                  = "eventhubcheckpoint"
-  storage_account_name  = azurerm_storage_account.secondary.name
-  container_access_type = "private"
-}
 resource "azurerm_container_app_environment" "primary" {
   name                       = "ace-${random_string.unique.result}-${local.loc_for_naming}"
   location                   = azurerm_resource_group.rg.location
@@ -205,4 +193,321 @@ resource "azurerm_container_app_environment" "secondary" {
   tags = local.tags
 
 }
+
+
+resource "azurerm_container_app" "producer" {
+  name                         = "ehfoproducer"
+  container_app_environment_id = azurerm_container_app_environment.primary.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
+
+  template {
+    container {
+      name   = "producer"
+      image  = "ghcr.io/implodingduck/eventhub-failover-producer:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name = "AZURE_CLIENT_ID"
+        value = azurerm_user_assigned_identity.this.client_id
+      }
+
+      env {
+        name = "EVENT_HUB_FULLY_QUALIFIED_NAMESPACE"
+        value = "ehnfailovertest${random_string.unique.result}.servicebus.windows.net"
+      }
+
+      env {
+        name = "EVENT_HUB_NAME"
+        value = "${azurerm_eventhub.primary.name}"
+      }
+
+    }
+    http_scale_rule {
+      name                = "http-1"
+      concurrent_requests = "100"
+    }
+    min_replicas = 0
+    max_replicas = 1
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 80
+    transport                  = "auto"
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+  
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.this.id]
+  }
+  tags = local.tags
+
+}
+
+resource "azurerm_container_app" "consumer" {
+  name                         = "ehfoconsumeralias"
+  container_app_environment_id = azurerm_container_app_environment.primary.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
+
+  template {
+    container {
+      name   = "consumer"
+      image  = "ghcr.io/implodingduck/eventhub-failover-consumer:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name = "AZURE_CLIENT_ID"
+        value = azurerm_user_assigned_identity.this.client_id
+      }
+      env {
+        name = "EVENT_HUB_FULLY_QUALIFIED_NAMESPACE"
+        value = "ehnfailovertest${random_string.unique.result}.servicebus.windows.net"
+      }
+
+      env {
+        name = "EVENT_HUB_NAME"
+        value = "${azurerm_eventhub.primary.name}"
+      }
+      env {
+        name = "EVENT_HUB_CONSUMER_GROUP"
+        value = "alias"
+      }
+
+      env {
+        name = "BLOB_STORAGE_ACCOUNT_URL"
+        value = "${azurerm_storage_account.primary.primary_blob_endpoint}"
+      }
+
+      env {
+        name = "BLOB_CONTAINER_NAME"
+        value = "eventhubcheckpoint"
+      }
+
+    }
+   
+    min_replicas = 1
+    max_replicas = 1
+  }
+
+  
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.this.id]
+  }
+  tags = local.tags
+
+}
+
+resource "azurerm_container_app" "primary" {
+  name                         = "ehfoconsumerprimary"
+  container_app_environment_id = azurerm_container_app_environment.primary.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
+
+  template {
+    container {
+      name   = "consumer"
+      image  = "ghcr.io/implodingduck/eventhub-failover-consumer:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name = "AZURE_CLIENT_ID"
+        value = azurerm_user_assigned_identity.this.client_id
+      }
+      env {
+        name = "EVENT_HUB_FULLY_QUALIFIED_NAMESPACE"
+        value = "${azurerm_eventhub_namespace.primary.name}.servicebus.windows.net"
+      }
+
+      env {
+        name = "EVENT_HUB_NAME"
+        value = "${azurerm_eventhub.primary.name}"
+      }
+      env {
+        name = "EVENT_HUB_CONSUMER_GROUP"
+        value = "primary"
+      }
+
+      env {
+        name = "BLOB_STORAGE_ACCOUNT_URL"
+        value = "${azurerm_storage_account.primary.primary_blob_endpoint}"
+      }
+
+      env {
+        name = "BLOB_CONTAINER_NAME"
+        value = "eventhubcheckpoint"
+      }
+
+    }
+   
+    min_replicas = 1
+    max_replicas = 1
+  }
+
+  
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.this.id]
+  }
+  tags = local.tags
+
+}
+
+resource "azurerm_container_app" "secondary" {
+  name                         = "ehfoconsumersecondary"
+  container_app_environment_id = azurerm_container_app_environment.primary.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
+
+  template {
+    container {
+      name   = "consumer"
+      image  = "ghcr.io/implodingduck/eventhub-failover-consumer:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name = "AZURE_CLIENT_ID"
+        value = azurerm_user_assigned_identity.this.client_id
+      }
+      env {
+        name = "EVENT_HUB_FULLY_QUALIFIED_NAMESPACE"
+        value = "${azurerm_eventhub_namespace.secondary.name}.servicebus.windows.net"
+      }
+
+      env {
+        name = "EVENT_HUB_NAME"
+        value = "${azurerm_eventhub.primary.name}"
+      }
+      env {
+        name = "EVENT_HUB_CONSUMER_GROUP"
+        value = "secondary"
+      }
+
+      env {
+        name = "BLOB_STORAGE_ACCOUNT_URL"
+        value = "${azurerm_storage_account.primary.primary_blob_endpoint}"
+      }
+
+      env {
+        name = "BLOB_CONTAINER_NAME"
+        value = "eventhubcheckpoint"
+      }
+
+    }
+   
+    min_replicas = 1
+    max_replicas = 1
+  }
+
+  
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.this.id]
+  }
+  tags = local.tags
+
+}
+
+resource "azurerm_container_app" "replicator" {
+  name                         = "ehfoconsumerreplicator"
+  container_app_environment_id = azurerm_container_app_environment.primary.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  workload_profile_name        = "Consumption"
+
+  template {
+    container {
+      name   = "consumer"
+      image  = "ghcr.io/implodingduck/eventhub-failover-replicator:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name = "AZURE_CLIENT_ID"
+        value = azurerm_user_assigned_identity.this.client_id
+      }
+      env {
+        name = "EVENT_HUB_FULLY_QUALIFIED_NAMESPACE_PRIMARY"
+        value = "${azurerm_eventhub_namespace.primary.name}.servicebus.windows.net"
+      }
+
+      env {
+        name = "EVENT_HUB_FULLY_QUALIFIED_NAMESPACE_SECONDARY"
+        value = "${azurerm_eventhub_namespace.secondary.name}.servicebus.windows.net"
+      }
+
+      env {
+        name = "EVENT_HUB_NAME"
+        value = "${azurerm_eventhub.primary.name}"
+      }
+      env {
+        name = "EVENT_HUB_CONSUMER_GROUP"
+        value = "replicator"
+      }
+
+      env {
+        name = "BLOB_STORAGE_ACCOUNT_URL"
+        value = "${azurerm_storage_account.primary.primary_blob_endpoint}"
+      }
+
+      env {
+        name = "BLOB_CONTAINER_NAME"
+        value = "eventhubcheckpoint"
+      }
+
+    }
+   
+    min_replicas = 1
+    max_replicas = 1
+  }
+
+  
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.this.id]
+  }
+  tags = local.tags
+
+}
+
+
+resource azurerm_user_assigned_identity "this" {
+  name                = "uai-ehfailovertest-${random_string.unique.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource azurerm_role_assignment "ehowner" {
+  principal_id = azurerm_user_assigned_identity.this.principal_id
+  role_definition_name = "Azure Event Hubs Data Owner"
+  scope = azurerm_resource_group.rg.id
+}
+resource azurerm_role_assignment "ehowner2" {
+  principal_id = azurerm_user_assigned_identity.this.principal_id
+  role_definition_name = "Azure Event Hubs Data Owner"
+  scope = azurerm_resource_group.rg2.id
+}
+
+resource azurerm_role_assignment "blobowner" {
+  principal_id = azurerm_user_assigned_identity.this.principal_id
+  role_definition_name = "Storage Blob Data Owner"
+  scope = azurerm_resource_group.rg.id
+}
+
 
